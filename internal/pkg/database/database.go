7c11870abb6a4b1ec43847545a692bb38ba278b9
Arman Tarkhanian <armantarkhanian@gmail.com>
2022-10-26 10:18:58 +0300

    refactoring

--- internal/pkg/database/database.go
+++ internal/pkg/database/database.go
@@ -53,10 +53,9 @@ func (db *Database) ReadConfig() (*config.Config, error) {
 	return &config, nil
 }
 
-func (db *Database) InsertUser(username string, email string, passwordBcrypt string) (userID int64, err error) {
+func (db *Database) InsertUser(username, email, passwordBcrypt string) (userID int64, err error) {
 	query := `INSERT INTO users (username, email, password_bcrypt, ranking) VALUES ($1, $2, $3, 400) RETURNING id;`
-	err = db.db.QueryRow(query, username, email, passwordBcrypt).Scan(&userID)
-	if err != nil {
+	if err = db.db.QueryRow(query, username, email, passwordBcrypt).Scan(&userID); err != nil {
 		if pgxErr, ok := err.(*pgconn.PgError); ok {
 			if pgxErr.ColumnName == "username" && pgxErr.Code == pgerrcode.UniqueViolation {
 				return 0, apierror.ErrorUsernameIsTaken
