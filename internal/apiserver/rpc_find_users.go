9c089aff7d8a2808888f94a0743c00557f36f746
Arman Tarkhanian <armantarkhanian@gmail.com>
2022-10-28 09:14:11 +0300

    rpc method find_users

---
+++ internal/apiserver/rpc_find_users.go
@@ -0,0 +1,53 @@
+package apiserver
+
+import (
+	"encoding/json"
+	"errors"
+	"strings"
+
+	"github.com/armantarkhanian/websocket"
+	"github.com/centrifugal/centrifuge"
+	"github.com/renju24/backend/internal/pkg/apierror"
+)
+
+type RPCFindUserRequest struct {
+	Username string `json:"username"`
+}
+
+type RPCFindUserResponse struct {
+	Users []findUser `json:"users"`
+}
+
+type findUser struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Ranking  int    `json:"ranking"`
+}
+
+func (app *APIServer) FindUsers(c *websocket.Client, jsonData []byte) (*RPCFindUserResponse, error) {
+	var req RPCFindUserRequest
+	if err := json.Unmarshal(jsonData, &req); err != nil {
+		return nil, centrifuge.ErrorBadRequest
+	}
+	req.Username = strings.TrimSpace(req.Username)
+	if req.Username == "" {
+		return nil, centrifuge.ErrorBadRequest
+	}
+	users, err := app.db.FindUsers(req.Username)
+	if err != nil {
+		if errors.Is(err, apierror.ErrorUserNotFound) {
+			return nil, apierror.ErrorUserNotFound
+		}
+		app.logger.Error().Err(err).Send()
+		return nil, centrifuge.ErrorInternal
+	}
+	var response RPCFindUserResponse
+	for _, user := range users {
+		response.Users = append(response.Users, findUser{
+			ID:       user.ID,
+			Username: user.Username,
+			Ranking:  user.Ranking,
+		})
+	}
+	return &response, nil
+}
